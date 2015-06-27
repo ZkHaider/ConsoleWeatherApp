038ec8656cc263a1ede0fcb6911335aa879ce6fc
ZkHaider <hkhan@oglethorpe.edu>
2015-06-27 00:07:14 -0400

    Initial Commit

---
+++ DefiningClasses/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DefiningClasses
+//
+//  Created by Haider Khan on 6/27/15.
+//  Copyright (c) 2015 ZkHaider. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
